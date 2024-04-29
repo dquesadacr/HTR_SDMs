730b92f9d7b4ef43860b6bfd99265d70e60d237e
dquesadacr <dannell.quesada@outlook.com>
2024-04-30 00:20:53 +0200

    fix logs folder

--- prepare.sh
+++ prepare.sh
@@ -15,9 +15,15 @@ rm preds.zip
 export repo="HTR_SDMs"
 git clone --depth 1 https://github.com/dquesadacr/"$repo".git
 
+cp HTR_SDMs/train_eval_predict.sb ./
+cp HTR_SDMs/run_colinvar.sh ./
+
+rm -rf HTR_SDMs
+mkdir -p logs
+
 bash train_eval_predict.sb Full
 bash train_eval_predict.sb LatLon "_latlon"
 
 ###
 
-sbatch --nodes=1 -c 16 --mem=58GB --qos=medium -J brms_SchoD -o ./jobs/brms_SchoD_%j.out -e ./jobs/brms_SchoD_%j.err --mail-user dannell.quesada@tu-dresden.de --mail-type END brms_SchoD_plots.sh "Full/C1_F10" "$cont"
+sbatch --nodes=1 -c 16 --mem=58GB --qos=medium -J brms_SchoD -o ./logs/brms_SchoD_%j.out -e ./logs/brms_SchoD_%j.err --mail-user dannell.quesada@tu-dresden.de --mail-type END brms_SchoD_plots.sh "Full/C1_F10" "$cont"
